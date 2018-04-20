4a90f6b92b190a2842d9351eaf263237a7dba478
jonathan olea <joleaz1200@alumno.ipn.mx>
2018-04-20 01:02:02 -0500

    'Script que inserta catalogo de Deptos y Jefes de éstos'

---
+++ Casos de Uso/CU no 2.3 Generar Solicitud Licencia por Paternidad/registros.sql
@@ -0,0 +1,200 @@
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (1, 'Lic. Andrés', null, 'Ortigoza', 'Campos');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (1, 'Dirección', 1);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (2, 'M. en C. Iván Giovanny', 1, 'Mosso', 'García');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (2, 'Subdirección Académica', 2);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (3, 'Dra. Blanca Esther', 1, 'Carvajal', 'Gámez');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (3, 'Sección de Estudios de Posgrado e Investigación', 3);
+
+
+
+/*INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (3, 'Lic. Andrés', 1, 'Ortigoza', 'Campos');*/
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (4, 'Subdirección de Servicios Educativos e Integración Social', 1);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (5, 'Ing. Felipe de Jesús', 1, 'Figueroa', 'Del Prado');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (5, 'Subdirección Administrativa', 5);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (6, 'M. en P. Laura', 1, 'Lazcano', 'Xoxotla');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (6, 'Coordinación de Enlace y Gestión Técnica', 6);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (7, 'M. en D. Juan Jesús', 1, 'Alcaraz', 'Torres');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (7, 'Unidad de Informática', 7);
+
+
+/*Basicas*/
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (8, 'Dra. Claudia Celia', 2, 'Díaz', 'Huerta');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (8, 'Formación Básica', 8);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (9, 'M. en C. Raúl', 2, 'Santillán', 'Luna');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (9, 'Ciencias e Ingeniería de la Computación', 9);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (10, 'M. en C. César', 2, 'Mújica', 'Ascencio');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (10, 'Ingeniería en Sistemas Computacionales', 10);
+
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (11, 'M. en C. Marisol', 2, 'Rodríguez', 'Ordaz');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (11, 'Formación Integral e Institucional', 11);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (12, 'Lic. Pablo', 2, 'Álvarez', 'Velázquez');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (12, 'Evaluación y Seguimiento Académico', 12);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (13, 'M. en C. Ricardo Felipe', 2, 'Díaz', 'Santiago');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (13, 'Innovación Educativa', 13);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (14, 'M. en C. Ricardo Felipe', 2, 'Díaz', 'Santiago');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (14, 'Tecnología Educativa y Campus Virtual', 14);
+
+
+/*SEPI*/
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (15, 'M. en C. Jesús Alfredo', 3, 'Martínez', 'Nuño');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (15, 'Departamento de Posgrado', 15);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (16, 'Dr. Manuel', 3, 'Salazar', 'Ramírez');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (16, 'Departamento de Investigación', 16);
+
+
+/*SSEIS*/
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (17, 'C. María Guadalupe', 4, 'Caro', 'García');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (17, 'Departamento de Gestión Escolar', 17);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (18, 'C. P. Fanny', 4, 'Sosa', 'Adán');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (18, 'Departamento de Servicios Estudiantiles', 18);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (19, 'Lic. José Francisco', 4, 'Serrano', 'García');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (19, 'Departamento de Extensión y Apoyos Educativos', 19);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (20, 'Ing. Patricia', 4, 'Escamilla', 'Miranda');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (20, 'Unidad Politécnica de Integración Social', 20);
+
+
+/*Subd Adva*/
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (21, 'Ing. Tania', 5, 'Rodríguez', 'Sarabia');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (21, 'Departamento de Capital Humano', 21);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (22, 'C. P. Patricia', 5, 'Mata', 'Gil');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (22, 'Departamento de Recursos Financieros', 22);
+
+
+INSERT INTO public.jefesuperior (idsuperior, nombre, jefe, apellidopaterno, apellidomaterno)
+VALUES (23, 'M. en C. Ricardo Ángel', 5, 'Aguilar', 'Pérez');
+
+INSERT INTO public.departamento (iddepartamento, nombre, idsuperior)
+VALUES (23, 'Departamento de Recursos Materiales y Servicios', 23);
+
+
+
+
+
+
+
+/*registros de profesores*/
+
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (11,1,'Ariel', 'López', 'Rojas', 9, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (12,2, 'Axel Ernesto', 'Moreno',' Cervantes', 10, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (13,3, 'Benjamin', 'Luna','Benoso',8, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (14,4, 'Carlos', 'Silva','Sánchez',8, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (15,5, 'César', 'Mujica','Ascencio',10, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (16,6, 'César Roman', 'Martínez','García',8, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (17,7, 'Chadwick', 'Carreto','Arellano',9, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (18,8, 'Consuelo Varinia', 'García','Mendoza',9, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (19,9, 'Edgardo Adrián', 'Franco','Martínez',9, 'DO');
+INSERT INTO public.personal (noempleado, notarjeta, nombre, apellidopaterno, apellidomaterno, iddepartamento, tipo)
+VALUES (20,10, 'José Jaime', 'López','Rabadán',10, 'DO');
