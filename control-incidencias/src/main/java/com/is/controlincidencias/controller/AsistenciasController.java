78bca9ffbdd8a5688747efba88ae35e22cc53d2c
Tonatihu <carlostonatihu@gmail.com>
2018-04-23 12:51:53 -0500

    Agregue una barra vertical para asistencias

--- control-incidencias/src/main/java/com/is/controlincidencias/controller/AsistenciasController.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/controller/AsistenciasController.java
@@ -8,14 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/asistencias")
 public class AsistenciasController {
     private static final String REGISTRAR_ASISTENCIA = "registrar-asistencia";
+    private static final String MODIFICAR_ASISTENCIA = "modificar-asistencia";
+    private static final String MOSTRAR_ASISTENCIA = "mostrar-asistencia";
+    private static final String ELIMINAR_ASISTENCIA = "eliminar-asistencia";
 
-    @GetMapping("")
-    public String asistencia() {
-        return "inicio";
+
+    @GetMapping({"", "/"})
+    public String inicio() {
+        return "inicio-asistencias";
     }
 
     @GetMapping("/registrar")
     public String registrar() {
         return REGISTRAR_ASISTENCIA;
+    }
+
+    @GetMapping("/modificar")
+    public String modificar() {
+        return MODIFICAR_ASISTENCIA;
+    }
+
+    @GetMapping("/mostrar")
+    public String mostrar() {
+        return MOSTRAR_ASISTENCIA;
+    }
+
+    @GetMapping("/eliminar")
+    public String eliminar() {
+        return ELIMINAR_ASISTENCIA;
     }
 }
