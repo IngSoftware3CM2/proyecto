4975bcc9232c550b3638b05195c217720bd4e524
jonathan olea <joleaz1200@alumno.ipn.mx>
2018-04-23 16:59:28 -0500

    Diagramas de CU 2.1 Iniciar sesión

---
+++ Casos de Uso/CU no 2.1 Iniciar Sesion/script.sql
@@ -0,0 +1,38 @@
+﻿-- =============================================================================
+-- Diagram Name: Diagrama R
+-- Created on: 23/04/2018 16:25:44
+-- Diagram Version: 
+-- =============================================================================
+
+
+DROP TABLE IF EXISTS "login" CASCADE;
+
+CREATE TABLE "login" (
+	"correo" varchar(70),
+	"idusuario" int4 NOT NULL,
+	"passwordsalt" varchar(255),
+	"passwordhash" varchar(255),
+	"PersonalnoEmpleado" int4 NOT NULL,
+	PRIMARY KEY("idusuario")
+);
+
+DROP TABLE IF EXISTS "personal" CASCADE;
+
+CREATE TABLE "personal" (
+	"nombre" varchar(60),
+	"apellidopaterno" varchar(30),
+	"apellidomaterno" varchar(60),
+	"noempleado" int4 NOT NULL,
+	"idusuario" int4,
+	PRIMARY KEY("noempleado")
+);
+
+
+ALTER TABLE "personal" ADD CONSTRAINT "Ref_Personal_to_Login" FOREIGN KEY ("idusuario")
+	REFERENCES "login"("idusuario")
+	MATCH SIMPLE
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+	NOT DEFERRABLE;
+
+
