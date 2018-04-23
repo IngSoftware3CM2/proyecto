92e5be3134b7307f17e9fdacad0b0a01a91cd0a1
Tonatihu <carlostonatihu@gmail.com>
2018-04-23 13:24:12 -0500

    Más vistas para el sistema de asistencias

--- control-incidencias/src/main/java/com/is/controlincidencias/controller/AsistenciasController.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/controller/AsistenciasController.java
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/asistencias")
 public class AsistenciasController {
-    private static final String REGISTRAR_ASISTENCIA = "registrar-asistencia";
-    private static final String MODIFICAR_ASISTENCIA = "modificar-asistencia";
-    private static final String MOSTRAR_ASISTENCIA = "mostrar-asistencia";
-    private static final String ELIMINAR_ASISTENCIA = "eliminar-asistencia";
+    private static final String INICIO_ASISTENCIAS = "inicio-asistencias";
+    private static final String REGISTRAR_ASISTENCIA = "registrar-asistencias";
+    private static final String MODIFICAR_ASISTENCIA = "modificar-asistencias";
+    private static final String MOSTRAR_ASISTENCIA = "mostrar-asistencias";
+    private static final String ELIMINAR_ASISTENCIA = "eliminar-asistencias";
 
 
     @GetMapping({"", "/"})
     public String inicio() {
-        return "inicio-asistencias";
+        return INICIO_ASISTENCIAS;
     }
 
     @GetMapping("/registrar")
