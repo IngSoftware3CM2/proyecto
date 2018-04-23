5b37236fe39b9c1589ca457c96f419ae5fcb8eb0
jonathan olea <joleaz1200@alumno.ipn.mx>
2018-04-23 18:43:14 -0500

    Mapeo para CU 2.1 Iniciar sesión, y datos para la BD

--- Datos/inserciones.sql
+++ Datos/inserciones.sql
@@ -1,11 +1,17 @@
 COPY jefesuperior(idsuperior,nombre,apellidopaterno,apellidomaterno,jefe) 
-FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\jefesuperior.csv' DELIMITER ',' CSV HEADER;
-	
+FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\jefesuperior.csv' DELIMITER ',' CSV HEADER;
+
+COPY departamento(iddepartamento,nombre,idsuperior) 
+FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\departamento.csv' DELIMITER ',' CSV HEADER;
+
+COPY personal(noempleado,nombre,apellidopaterno,apellidomaterno,notarjeta,iddepartamento,tipo) 
+FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\personal.csv' DELIMITER ','CSV HEADER;
+
 COPY incidencia(idincidencia,fecharegistro,tipo,idjustificante,noempleado,idquincena)
-FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\incidencia.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\incidencia.csv' DELIMITER ',' CSV HEADER;
 
 COPY quincena(idquincena,inicio,fin)
-FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\quincena.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\quincena.csv' DELIMITER ',' CSV HEADER;
 
 COPY asistencia(idasistencia,fecharegistro,horaentrada,horasalida,noempleado)
-FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\asistencia.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\asistencia.csv' DELIMITER ',' CSV HEADER;
---
+++ control-incidencias/src/main/java/com/is/controlincidencias/entity/Login.java
@@ -0,0 +1,54 @@
+package com.is.controlincidencias.entity;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "login")
+public class Login {
+    @Id
+    @Column(name = "correo", nullable = false)
+    private String correo;
+
+    @Column(name = "passwordsalt", nullable = false, length = 255)
+    private String passwordsalt;
+
+    @Column(name = "passwordhash", nullable = false, length = 255)
+    private String passwordhash;
+
+
+    private static final String definition = "FOREIGN KEY(noEmpleado) REFERENCES personal (noEmpleado) ON UPDATE CASCADE ON DELETE CASCADE";
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "noEmpleado", foreignKey = @ForeignKey(name = "empleadoLogin_fk", foreignKeyDefinition = definition))
+
+    private Personal personal;
+
+
+    public Login(String passwordsalt, String passwordhash, Personal personal) {
+        this.passwordsalt = passwordsalt;
+        this.passwordhash = passwordhash;
+        this.personal = personal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Login)) return false;
+        Login login = (Login) o;
+        return Objects.equals(correo, login.correo) &&
+                Objects.equals(passwordsalt, login.passwordsalt) &&
+                Objects.equals(passwordhash, login.passwordhash) &&
+                Objects.equals(personal, login.personal);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(correo, passwordsalt, passwordhash, personal);
+    }
+
+
+
+}
