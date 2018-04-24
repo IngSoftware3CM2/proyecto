80b81edb4538aafa09bc0f1fd37aa09f092dfc57
PitCoder <overlord.lae@gmail.com>
2018-04-24 15:16:16 -0500

    Ajuste de los datos

--- Datos/inserciones.sql
+++ Datos/inserciones.sql
@@ -1,17 +1,23 @@
+COPY zona(idZona,nombre) 
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\zona.csv' DELIMITER ',' CSV HEADER;
+
+COPY unidadmedica(idUnidad,Nombre,idZona) 
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\unidadmedica.csv' DELIMITER ',' CSV HEADER;
+
 COPY jefesuperior(idsuperior,nombre,apellidopaterno,apellidomaterno,jefe) 
-FROM '/home/abhera/Documentos/PROYECTOING/proyecto/Datos/jefesuperior.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\jefesuperior.csv' DELIMITER ',' CSV HEADER;
 
 COPY departamento(iddepartamento,nombre,idsuperior) 
-FROM '/home/abhera/Documentos/PROYECTOING/proyecto/Datos/departamento.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\departamento.csv' DELIMITER ',' CSV HEADER;
 
 COPY personal(noempleado,nombre,apellidopaterno,apellidomaterno,notarjeta,iddepartamento,tipo) 
-FROM '/home/abhera/Documentos/PROYECTOING/proyecto/Datos/personal.csv' DELIMITER ','CSV HEADER;
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\personal.csv' DELIMITER ','CSV HEADER;
 
 COPY quincena(idquincena,inicio,fin)
-FROM '/home/abhera/Documentos/PROYECTOING/proyecto/Datos/quincena.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\quincena.csv' DELIMITER ',' CSV HEADER;
 
 COPY incidencia(idincidencia,fecharegistro,tipo,idjustificante,noempleado,idquincena)
-FROM '/home/abhera/Documentos/PROYECTOING/proyecto/Datos/incidencia.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\incidencia.csv' DELIMITER ',' CSV HEADER;
 
 COPY asistencia(idasistencia,fecharegistro,horaentrada,horasalida,noempleado)
-FROM '/home/abhera/Documentos/PROYECTOING/proyecto/Datos/asistencia.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\HeadHunters\Desktop\Documents\GitHub\proyecto\Datos\asistencia.csv' DELIMITER ',' CSV HEADER;
