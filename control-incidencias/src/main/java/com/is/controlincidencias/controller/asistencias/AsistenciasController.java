69c8ff67fe87d05a4e397381387ce5575b5acdff
Tonatihu <carlostonatihu@gmail.com>
2018-04-28 21:55:14 -0500

    Modificar asistencia sin validaciones de dia

--- control-incidencias/src/main/java/com/is/controlincidencias/controller/asistencias/AsistenciasController.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/controller/asistencias/AsistenciasController.java
@@ -1,18 +1,34 @@
 package com.is.controlincidencias.controller.asistencias;
 
-import com.is.controlincidencias.model.AsistenciaJSON;
+import com.is.controlincidencias.entity.Asistencia;
+import com.is.controlincidencias.model.AsistenciaForm;
+import com.is.controlincidencias.service.AsistenciaService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/asistencias")
 public class AsistenciasController {
+    private static final Log LOG = LogFactory.getLog(AsistenciasController.class);
     private static final String INICIO_ASISTENCIAS = "inicio-asistencias";
     private static final String REGISTRAR_ASISTENCIA = "registrar-asistencias";
     private static final String MODIFICAR_ASISTENCIA = "modificar-asistencias";
     private static final String MOSTRAR_ASISTENCIA = "mostrar-asistencias";
     private static final String ELIMINAR_ASISTENCIA = "eliminar-asistencias";
+    private static final String RESULTADO = "resultado";
+    private static final String MODELO = "modelo";
+
+    @Autowired
+    @Qualifier("asistenciaServiceImpl")
+    private AsistenciaService asistenciaService;
 
 
     @GetMapping({"", "/"})
@@ -25,10 +41,61 @@ public class AsistenciasController {
         return REGISTRAR_ASISTENCIA;
     }
 
-    @RequestMapping("/modificar")
-    public String modificar(Model model) {
-        AsistenciaJSON asistencia = new AsistenciaJSON();
-        model.addAttribute("asistenciaModel", asistencia);
+    @PostMapping(params = "modificar", value = "/modificar")
+    public String modificarPOST(@ModelAttribute(name = "modelo") AsistenciaForm modelo, Model model) {
+        LOG.info("modificarPOST()");
+        int resultado = 0;
+        AsistenciaForm asistencia = new AsistenciaForm();
+        if (validarDatos(modelo)) {
+            Asistencia a = asistenciaService.modificarAsistencia(modelo);
+            if (a != null)
+                resultado = 3;
+            else
+                resultado = 4;
+        } else
+            resultado = 4;
+
+
+        model.addAttribute(RESULTADO, resultado);
+        model.addAttribute(MODELO, asistencia);
+        LOG.info("saliendo de modificarPOST()");
+        return MODIFICAR_ASISTENCIA;
+    }
+
+    private boolean validarDatos(AsistenciaForm modelo) {
+        if (!modelo.getFecha().matches("^\\d{4}-\\d{2}-\\d{2}$"))
+            return false;
+        if (!modelo.getHoraEntrada().matches("^\\d{1,2}:\\d\\d$"))
+            return false;
+        return modelo.getHoraSalida().matches("^\\d{1,2}:\\d\\d$");
+    }
+
+    @PostMapping(params = "consultar", value = "/modificar")
+    public String consultarPOST(@ModelAttribute(name = "modelo") AsistenciaForm modelo, Model model) {
+        LOG.info("consultarPOST: ");
+        int resultado = 0;
+
+        AsistenciaForm asistencia = new AsistenciaForm();
+        resultado = asistenciaService.existeAsistencia(modelo);
+        if (resultado == 0) {
+            asistencia = asistenciaService.buscarAsistencia(modelo);
+        }
+        model.addAttribute(RESULTADO, resultado);
+        model.addAttribute(MODELO, asistencia);
+
+        LOG.info("saliendo de consultarPOST()");
+        return MODIFICAR_ASISTENCIA;
+    }
+
+    @GetMapping("/modificar")
+    public String modificarGET(Model model) {
+        LOG.info("modificarGET()");
+
+        AsistenciaForm asistencia = new AsistenciaForm();
+        model.addAttribute(MODELO, asistencia);
+        model.addAttribute(RESULTADO, 0);
+
+        LOG.info("saliendo de modificarGET()");
         return MODIFICAR_ASISTENCIA;
     }
 
--- control-incidencias/src/main/java/com/is/controlincidencias/service/impl/AsistenciaServiceImpl.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/service/impl/AsistenciaServiceImpl.java
@@ -2,6 +2,7 @@ package com.is.controlincidencias.service.impl;
 
 import com.is.controlincidencias.entity.Asistencia;
 import com.is.controlincidencias.entity.Personal;
+import com.is.controlincidencias.model.AsistenciaForm;
 import com.is.controlincidencias.model.AsistenciaJSON;
 import com.is.controlincidencias.repository.AsistenciaRepository;
 import com.is.controlincidencias.repository.PersonalRepository;
@@ -14,12 +15,15 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Service("asistenciaServiceImpl")
 public class AsistenciaServiceImpl implements AsistenciaService {
     private static final Log LOG = LogFactory.getLog(AsistenciaServiceImpl.class);
     private LocalTime siete = LocalTime.of(7, 0, 0, 0);
     private LocalTime veintidos = LocalTime.of(22,  0, 0, 0);
+    private DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private DateTimeFormatter formatterHour = DateTimeFormatter.ofPattern("HH:mm");
 
     @Autowired
     @Qualifier("asistenciaRepository")
@@ -55,5 +59,44 @@ public class AsistenciaServiceImpl implements AsistenciaService {
             asistencia.setHoraSalida(veintidos);
         Asistencia a = asistenciaRepository.save(asistencia);
         LOG.info(a.getIdAsistencia());
+    }
+
+    @Override
+    public int existeAsistencia(AsistenciaForm asistenciaForm) {
+        boolean existeTarjeta = personalRepository.existsPersonalByNoTarjeta(asistenciaForm.getTarjeta());
+        if (!existeTarjeta)
+            return 1; // No existe la tarjeta
+
+        LocalDate fecha = LocalDate.parse(asistenciaForm.getFecha(), formatterDate);
+        Asistencia a = asistenciaRepository
+                .findAsistenciaByFechaRegistroAndPersonal_NoTarjeta(fecha, asistenciaForm.getTarjeta());
+        if (a == null)
+            return 2; // No se encontro registro
+
+        return 0; // Salio chido
+    }
+
+    @Override
+    public AsistenciaForm buscarAsistencia(AsistenciaForm asistenciaForm) {
+        LocalDate fecha = LocalDate.parse(asistenciaForm.getFecha(), formatterDate);
+        Asistencia a = asistenciaRepository
+                .findAsistenciaByFechaRegistroAndPersonal_NoTarjeta(fecha, asistenciaForm.getTarjeta());
+
+        asistenciaForm.setHoraSalida(a.getHoraSalida().format(formatterHour));
+        asistenciaForm.setHoraEntrada(a.getHoraEntrada().format(formatterHour));
+
+        return asistenciaForm;
+    }
+
+    @Override
+    public Asistencia modificarAsistencia(AsistenciaForm asistenciaForm) {
+        LocalDate fecha = LocalDate.parse(asistenciaForm.getFecha(), formatterDate);
+        LocalTime horaEntrada = LocalTime.parse(asistenciaForm.getHoraEntrada(), formatterHour);
+        LocalTime horaSalida = LocalTime.parse(asistenciaForm.getHoraSalida(), formatterHour);
+        Asistencia a = asistenciaRepository
+                .findAsistenciaByFechaRegistroAndPersonal_NoTarjeta(fecha, asistenciaForm.getTarjeta());
+        a.setHoraEntrada(horaEntrada);
+        a.setHoraSalida(horaSalida);
+        return asistenciaRepository.save(a);
     }
 }
