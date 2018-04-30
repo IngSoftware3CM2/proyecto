d9bbeb9225428a8a08d6dd8cc4bf2e3793c2bca6
“JoseRicardoL” <logjose.ri@gmail.com>
2018-04-30 02:00:37 -0500

    Quien tiro esto >:v

--- Datos/inserciones.sql
+++ Datos/inserciones.sql
@@ -1,23 +1,23 @@
 COPY zona(idZona,nombre) 
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\zona.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/zona.csv' DELIMITER ',' CSV HEADER;
 
 COPY unidadmedica(idUnidad,Nombre,idZona) 
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\unidadmedica.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/unidadmedica.csv' DELIMITER ',' CSV HEADER;
 
 COPY jefesuperior(idsuperior,nombre,apellidopaterno,apellidomaterno,jefe) 
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\jefesuperior.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/jefesuperior.csv' DELIMITER ',' CSV HEADER;
 
 COPY departamento(iddepartamento,nombre,idsuperior) 
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\departamento.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/departamento.csv' DELIMITER ',' CSV HEADER;
 
 COPY personal(noempleado,nombre,apellidopaterno,apellidomaterno,notarjeta,iddepartamento,tipo) 
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\personal.csv' DELIMITER ','CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/personal.csv' DELIMITER ','CSV HEADER;
 
 COPY quincena(idquincena,inicio,fin)
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\quincena.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/quincena.csv' DELIMITER ',' CSV HEADER;
 
 COPY incidencia(idincidencia,fecharegistro,tipo,idjustificante,noempleado,idquincena)
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\incidencia.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/incidencia.csv' DELIMITER ',' CSV HEADER;
 
 COPY asistencia(idasistencia,fecharegistro,horaentrada,horasalida,noempleado)
-FROM 'C:\Users\jonat\Documents\GitHub\proyecto\Datos\asistencia.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/josericardol/IntellijProjects/proyecto/proyecto/Datos/asistencia.csv' DELIMITER ',' CSV HEADER;
