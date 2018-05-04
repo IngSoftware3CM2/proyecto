54a6562fcb29ba78dfd918044bc4ae955adf036a
Tonatihu <carlostonatihu@gmail.com>
2018-05-04 12:26:10 -0500

    :poop: Eliminando los import no necesarios

--- control-incidencias/src/main/java/com/is/controlincidencias/controller/CambioHorarioController.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/controller/CambioHorarioController.java
@@ -7,7 +7,6 @@ import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
--- control-incidencias/src/main/java/com/is/controlincidencias/model/JustificanteModel.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/model/JustificanteModel.java
@@ -1,6 +1,5 @@
 package com.is.controlincidencias.model;
 
-import javax.persistence.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
