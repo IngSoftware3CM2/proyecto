c9149357165daa73d2dfccd64f2fd309b619b5fd
Tonatihu <carlostonatihu@gmail.com>
2018-05-08 07:08:43 -0500

    Maldita sea, revisen su codigo

--- control-incidencias/src/main/java/com/is/controlincidencias/controller/LicenciaPaternidadController.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/controller/LicenciaPaternidadController.java
@@ -5,7 +5,6 @@ import com.is.controlincidencias.constants.Constants;
 import com.is.controlincidencias.entity.Incidencia;
 import com.is.controlincidencias.entity.LicPaternidad;
 import com.is.controlincidencias.entity.Personal;
-import com.is.controlincidencias.entity.Justificante;
 import com.is.controlincidencias.model.LicPaternidadModel;
 import com.is.controlincidencias.service.IncidenciaService;
 import com.is.controlincidencias.service.JustificanteService;
@@ -20,9 +19,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.Principal;
 import java.util.List;
 
@@ -31,7 +27,8 @@ import java.util.List;
 
 public class LicenciaPaternidadController {
 
-    int idIncidencia, noEmpleado;
+    int idIncidencia;
+    int noEmpleado;
     LicPaternidad licPaternidadModificar = new LicPaternidad();
 
     @Autowired
@@ -54,19 +51,18 @@ public class LicenciaPaternidadController {
 
     @GetMapping("/cancel")
     private String cancel(){
-        //return "redirect:/contacts/showcontacts";
         return "ver-incidencias";
     }
 
     @GetMapping("/agregar")
-    private String RedirectSolicitudLicenciaPaternidadForm(Model model, @RequestParam(name="id")Integer idincidencia, Principal principal) {
+    private String redirectSolicitudLicenciaPaternidadForm(Model model, @RequestParam(name="id")Integer idincidencia, Principal principal) {
         String email = "";
         if (principal!=null && principal.getName()!=null){
             email=principal.getName();
         }
         Personal personal = personalService.getPersonalByEmail(email);
         LicPaternidadModel licPaternidadModel = new LicPaternidadModel();
-        idIncidencia=idincidencia;
+        idIncidencia = idincidencia;
         Incidencia incidencia = incidenciaService.consultarIncidencia(idincidencia);
         model.addAttribute("licPaternidadModel", licPaternidadModel);
         noEmpleado = personal.getNoEmpleado();
@@ -77,7 +73,7 @@ public class LicenciaPaternidadController {
 
 
     @GetMapping("/modificar")
-    private String RedirectSolicitudLicenciaPaternidadFormModificar(Model model,@RequestParam(name="idjustificante")Integer idjustificante) {
+    private String redirectSolicitudLicenciaPaternidadFormModificar(Model model,@RequestParam(name="idjustificante")Integer idjustificante) {
         if (idjustificante!=0){
             LicPaternidad licPaternidad = licPaternidadService.buscarLicPaternidadPorIdjustificante(idjustificante);
             LicPaternidadModel licPaternidadModel = new LicPaternidadModel();
@@ -99,7 +95,7 @@ public class LicenciaPaternidadController {
 
 
         @PostMapping("/add-lic-paternidad")
-    private String GuardarLicPaternidad(@ModelAttribute("licPaternidadModel") LicPaternidadModel licPaternidadModel,@RequestParam("file") List<MultipartFile> files) {
+    private String guardarLicPaternidad(@ModelAttribute("licPaternidadModel") LicPaternidadModel licPaternidadModel,@RequestParam("file") List<MultipartFile> files) {
         LOG.info("Datos que me llegan "+licPaternidadModel.toString());
         //Necesito crear un justificante, darlo de alte en la base y despues utilizarlo
         licPaternidadModel.setRegistrolicencia(files.get(0).getOriginalFilename());
@@ -112,7 +108,7 @@ public class LicenciaPaternidadController {
             int idjustificante = licPaternidadService.guardarLicPaternidad(licPaternidadModel, idIncidencia, noEmpleado);
             licPaternidadService.subirArchivo(files,idjustificante);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("ERROR:", e);
         }
         return "redirect:/personal/justificantes";
     }
@@ -121,9 +117,6 @@ public class LicenciaPaternidadController {
     private String updateLicPaternidad(@ModelAttribute("licPaternidadModel") LicPaternidadModel licPaternidadModel,@RequestParam("file") List<MultipartFile> files) {
         LOG.info("Datos que me llegan "+licPaternidadModel.toString());
         //Necesito crear un justificante, darlo de alte en la base y despues utilizarlo
-        if (true){
-
-        }
         licPaternidadModel.setRegistrolicencia(files.get(0).getOriginalFilename());
         licPaternidadModel.setActanacimiento(files.get(1).getOriginalFilename());
         licPaternidadModel.setActamatrimonio(files.get(2).getOriginalFilename());
