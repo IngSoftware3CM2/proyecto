1388029a556cdad423186e992a758380a8f4a2cd
samyarteaga <sam-y.barca@hotmail.com>
2018-05-09 11:24:48 -0500

    corrigiendo a ricardito

--- control-incidencias/src/main/java/com/is/controlincidencias/service/impl/PermisoEconomicoServiceImpl.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/service/impl/PermisoEconomicoServiceImpl.java
@@ -37,7 +37,7 @@ public class PermisoEconomicoServiceImpl implements PermisoEconomicoService {
     public int addPermisoEconomico(PermisoEconomicoModel permisoEconomicoModel, Justificante justificante) {
         Date fecha = new Date();
         int noEmpleado=justificante.getPersonal().getNoEmpleado();
-        justificanteRepository.altaJustificante("Espera",fecha,noEmpleado);
+        justificanteRepository.altaJustificante("Espera",fecha,3,noEmpleado);
         List<Integer> ids = justificanteRepository.ultimoJustificanteAnadido();
         LocalDate fechaIncidencia = StringToLocalDate.tryParseDate(permisoEconomicoModel.getFechaIncidencia());
         permisoEconomicoRepository.addPermisoEconomico(fechaIncidencia, ids.get(ids.size()-1));
