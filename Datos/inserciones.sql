2956a109be8409ad0fffed1a48ece5b7f1deebd3
jonathan olea <joleaz1200@alumno.ipn.mx>
2018-05-31 19:57:59 -0500

    se agregan atributos a Quincena, y sus registros respectivos

--- Datos/inserciones.sql
+++ Datos/inserciones.sql
@@ -13,7 +13,7 @@ FROM 'C:\Users\jonat\Documents\Auxiliar\proyecto\Datos\departamento.csv' DELIMIT
 COPY personal(idempleado,noempleado,activo,sexo,nombre,apellidopaterno,apellidomaterno,notarjeta,iddepartamento,tipo) 
 FROM 'C:\Users\jonat\Documents\Auxiliar\proyecto\Datos\personal.csv' DELIMITER ','CSV HEADER;
 
-COPY quincena(idquincena,inicio,fin)
+COPY quincena(idquincena,inicio,fin,quincenareportada,quincenaenqueseraprocesada,habil)
 FROM 'C:\Users\jonat\Documents\Auxiliar\proyecto\Datos\quincena.csv' DELIMITER ',' CSV HEADER;
 
 COPY incidencia(idincidencia,fecharegistro,tipo,idjustificante,noempleado,idquincena)
