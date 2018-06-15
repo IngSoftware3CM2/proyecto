a6dd67c33a8d8f6dbfbbc01a90853d1c41f463b5
jonathan olea <joleaz1200@alumno.ipn.mx>
2018-06-15 13:17:25 -0500

    personal tiene un booleano para saber si tiene horarioAbierto

--- control-incidencias/src/main/java/com/is/controlincidencias/entity/Personal.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/entity/Personal.java
@@ -34,6 +34,9 @@ public class Personal {
     @Column(name = "activo", nullable = false)  //se deja en false si es docente y se va de año sabático, eg,  para no generarle incidencias, para quien haga el cronos
     private Boolean activo;
 
+    @Column(name = "habierto", nullable = false)  //se deja en false si NO tiene horarioAbierto 
+    private Boolean habierto;
+
     @Column(name = "tipo", nullable = false, length = 10)
     private String tipo;
 
