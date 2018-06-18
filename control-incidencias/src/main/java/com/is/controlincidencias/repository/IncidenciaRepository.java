6530b6375702005ae97d303ba39a5e14e562e550
Tonatihu <carlostonatihu@gmail.com>
2018-06-18 00:04:22 -0500

    Se necesita calcular las horas tanto de falta como suplementarias

--- control-incidencias/src/main/java/com/is/controlincidencias/repository/IncidenciaRepository.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/repository/IncidenciaRepository.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository("incidenciaRepository")
@@ -28,4 +29,14 @@ public interface IncidenciaRepository extends JpaRepository<Incidencia, Serializ
     @Transactional
     @Query(value = "select idempleado from incidencia where idjustificante = :idjustificante", nativeQuery = true)
     public int getIdEmpleadoByIdJustificante(@Param("idjustificante") int idjustificante);
+
+    @Modifying
+    @Query(value = "insert into incidencia (idincidencia, fecharegistro, tipo, idempleado) values (:id, :fecha, :tipo, :empleado)", nativeQuery
+            = true)
+    @Transactional
+    void insertarAsistencia(@Param("id") Integer id, @Param("fecha") LocalDate fecha, @Param("tipo") String tipo, @Param("empleado") Integer empleado);
+
+    @Query(value = "select max(idincidencia) from incidencia", nativeQuery = true)
+    @Transactional
+    Integer obtenerMaximoIdAsistencia();
 }
