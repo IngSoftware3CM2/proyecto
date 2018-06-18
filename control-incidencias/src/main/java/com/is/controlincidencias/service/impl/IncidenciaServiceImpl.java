bc0efa0751afdc55c14f38ebba5d38a6ac341020
Tonatihu <carlostonatihu@gmail.com>
2018-06-17 20:30:45 -0500

    Chingo de trayectorias

--- control-incidencias/src/main/java/com/is/controlincidencias/service/impl/IncidenciaServiceImpl.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/service/impl/IncidenciaServiceImpl.java
@@ -38,6 +38,11 @@ public class IncidenciaServiceImpl implements IncidenciaService {
     @Qualifier("asistenciaRepository")
     private AsistenciaRepository asistenciaRepository;
 
+    private static final int MINUTO = 60;
+    private static final int TREINTA_MIN = MINUTO * 30;
+    private static final int TREINTA_UNO = MINUTO * 31;
+    private static final int ONCE = MINUTO * 11;
+
     @Override
     public List<Incidencia> getIncidenciasByPersonal(Personal personal) {
         return incidenciaRepository.findAllByPersonal(personal);
@@ -94,15 +99,13 @@ public class IncidenciaServiceImpl implements IncidenciaService {
 
         c.add(Calendar.DATE, -2);
         date = c.getTime();
-        log.info("The time for validation is {}", date);
-        log.info("The time for validation is {}", fecha);
         // Validar si esa fecha es un dia inhabil o fin de semana
 
         // Obtener el nombre del dia en el formato que esta en la base de datos (LUN, MAR, MIE, JUE,
         // VIE) para hacer la consulta
         String diaSemana = obtenerDia(fecha);
-        log.info("diaSemana = " + diaSemana);
         // Obtenemos todas las tuplas de personal
+        int hrEntrada, hrSalida, entradaHorario, salidaHorario;
         for(Personal per:listaPersonal){
             // Obtiene el numero de empleado y tipo de personal
             log.info("Nombre: "+per.getNombre()+" ID: "+per.getIdEmpleado()+ " Tipo: " +
@@ -112,17 +115,79 @@ public class IncidenciaServiceImpl implements IncidenciaService {
             // Obtiene el horario de dicho empleado de acuerdo a la RN48 (del dia previo al anterior)
             Dia dia = diaRepository.findFirstByHorarioActual_IdHorarioAndNombre(idHorario,
                     diaSemana);
-            log.info("idDia: "+dia.getIdDia()+" horaEntrada: "+dia.getHoraEntrada()+ " " + "horaSalida: "+dia.getHoraSalida());
 
             // Obtiene registro de entrada y de salida del empleado del dia que cumpla con la RN48
             // Uso esta madre porque ya la tenia xD
             Asistencia a = asistenciaRepository.findAsistenciaByFechaRegistroAndPersonalNoTarjeta
                     (fecha, per.getNoTarjeta());
-            log.info(a.toString());
             // Coteja las horas de entrada y salida del horario del empleado con las de su asistencia
 
             // En caso de haber inconsistencias identificar el tipo de incidencia, de acuerdo al tipo de personal y las
             // respectivas reglas del negocio
+            if (per.getTipo().equals("ROLE_DOC") || per.getTipo().equals("ROLE_DCADM")) {
+                // Trayectoria F
+                if (a == null || a.getHoraEntrada() == null || a.getHoraSalida() == null) {
+
+                    if (per.getTipo().equals("ROLE_DCADM")) {
+                        // Trayectoria G
+                    } else {
+                    }
+                    // Continua en 5 de la principal
+                    // continue;
+                } else {
+                    // Trayectoria A
+                    if (per.getHabierto()) {
+                        float resta = (float) a.getHoraSalida().toSecondOfDay() - a.getHoraEntrada()
+                                .toSecondOfDay();
+                        resta = resta / 3600f;
+                        if (resta < 8) {
+                            // Trayectoria I
+                            log.info("Te faltaron horas " + resta + " Trayectoria I");
+                        } else if (resta > 9) {
+                            // Trayectoria L
+                            log.info("Te pasaste de horas " + resta + " Trayectoria L");
+                        } else {
+                            // Trayectoria J
+                            log.info("TODO CHIDO - Trayectoria J");
+                        }
+                        // Continuar punto 5 trayectoria principal
+                        //continue;
+                    } else {
+                        boolean trayectoriaE =;
+                        if (trayectoriaE) {
+                            // Trayectoria E
+                            // 
+                        }
+                    }
+                }
+            } else if (per.getTipo().equals("ROLE_PAAE")) {
+                if (a == null || a.getHoraEntrada() == null || a.getHoraSalida() == null) {
+                    //Trayectoria D
+                } else {
+                    hrEntrada = a.getHoraEntrada().toSecondOfDay();
+                    hrSalida = a.getHoraSalida().toSecondOfDay();
+                    entradaHorario = dia.getHoraEntrada().toSecondOfDay();
+                    salidaHorario = dia.getHoraSalida().toSecondOfDay();
+                    boolean esTrayectoriaB = entradaHorario + MINUTO <= hrEntrada;
+                    esTrayectoriaB = esTrayectoriaB && hrEntrada <= entradaHorario + TREINTA_MIN;
+                    esTrayectoriaB = esTrayectoriaB && salidaHorario <= hrSalida;
+                    if (esTrayectoriaB) {
+                        // Trayectoria B
+                        // Continua 5 principal
+                        // continue;
+                    }
+                    boolean esTrayectoriaC = hrEntrada >= entradaHorario + TREINTA_UNO;
+                    esTrayectoriaC = esTrayectoriaC || hrSalida < salidaHorario;
+                    if (esTrayectoriaC) {
+                        // Trayectoria C
+                        // Continua en 5 de la principal
+                        // continue;
+                    }
+                }
+
+            }
+
+            break;
         }
 
         return 0;
