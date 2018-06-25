bbfdb546a10871688e92559082b4bd6c6f38d216
Tonatihu <carlostonatihu@gmail.com>
2018-06-25 15:09:36 -0500

    Redirect sin probar

--- control-incidencias/src/main/java/com/is/controlincidencias/controller/JustificantesController.java
+++ control-incidencias/src/main/java/com/is/controlincidencias/controller/JustificantesController.java
@@ -4,12 +4,15 @@ import com.is.controlincidencias.entity.Justificante;
 import com.is.controlincidencias.entity.Personal;
 import com.is.controlincidencias.service.impl.JustificanteServiceImpl;
 import com.is.controlincidencias.service.impl.PersonalServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -20,6 +23,7 @@ import java.util.List;
 * trabajaran en este controlador al cual redireccionaran los respectivas vistas de ver
 * justificantes */
 
+@Slf4j
 @Controller
 @RequestMapping("/justificantes")
 public class JustificantesController {
@@ -33,44 +37,60 @@ public class JustificantesController {
     private PersonalServiceImpl personalService;
 
     @GetMapping("/paternidad")
-    public String verPaternidad() {
+    public String verPaternidad(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/tipoa")
-    public String verTipoA() {
+    public String verTipoA(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/omision")
-    public String verOmision() {
+    public String verOmision(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/retardo")
-    public String verRetardo() {
+    public String verRetardo(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/cambiohorario")
-    public String verCambioHorario() {
+    public String verCambioHorario(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/economico")
-    public String verEconomico() {
+    public String verEconomico(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/suplementario")
-    public String verSuplementario() {
+    public String verSuplementario(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
 
     @GetMapping("/comision")
-    public String verComisionOficial() {
+    public String verComisionOficial(@RequestParam(name = "id") Integer idJustificante) {
         return "inicio";
     }
+
+    @GetMapping("/redirect}")
+    public String redirectJustificante(@RequestParam(name = "id") Integer id,
+            @RequestParam(name = "tipo") Integer tipo, RedirectAttributes attributes) {
+        log.info("redirectJustificante() id = " + id + " tipoJustificante = " + tipo);
+        attributes.addAttribute("id", id);
+        String redirectURL = "redirect:/personal";
+        // Faltan los demás pero no se los numeros
+        if (tipo == 1)
+            redirectURL = "redirect:/justifiantes/tipoa";
+        else if (tipo == 2)
+            redirectURL = "";
+
+        return redirectURL;
+    }
+
 
     @GetMapping("/validar")
     public ModelAndView validarJustificantes (Principal principal) {
